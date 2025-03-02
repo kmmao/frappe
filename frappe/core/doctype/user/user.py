0a3ae1567cadf8b72d8c21d3213235947e802430
allen <kmmao@outlook.com>
2025-03-02 16:42:25 +0800

    更新新的配置

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -119,7 +119,7 @@ class User(Document):
 		reset_password_key: DF.Data | None
 		restrict_ip: DF.SmallText | None
 		role_profile_name: DF.Link | None
-		role_select: DF.Literal["", "\u603b\u52a9", "\u8fd0\u8425\u7ecf\u7406", "\u8fd0\u8425\u5458", "\u4e1a\u52a1\u5408\u4f19\u4eba", "\u4e1a\u52a1\u652f\u6301", "\u4e1a\u52a1\u7ecf\u7406", "\u8d22\u52a1\u7ecf\u7406", "\u8d22\u52a1\u5458", "\u91c7\u9500\u7ecf\u7406", "\u91c7\u9500\u5458", "\u6570\u636e\u5458", "\u5ba2\u670d", "\u5e97\u957f", "\u5408\u4f19\u4eba", "\u4e1a\u4e3b", "\u7ef4\u4fee\u5458"]
+		role_select: DF.Literal["", "\u603b\u52a9", "\u8fd0\u8425\u7ecf\u7406", "\u8fd0\u8425\u5458", "\u5408\u4f19\u4eba", "\u4e1a\u52a1\u652f\u6301", "\u4e1a\u52a1\u7ecf\u7406", "\u8d22\u52a1\u7ecf\u7406", "\u8d22\u52a1\u5458", "\u91c7\u9500\u7ecf\u7406", "\u6570\u636e\u5458", "\u5ba2\u670d", "\u5e97\u957f", "\u4e1a\u4e3b", "\u7ef4\u4fee\u5458"]
 		roles: DF.Table[HasRole]
 		search_bar: DF.Check
 		send_me_a_copy: DF.Check
